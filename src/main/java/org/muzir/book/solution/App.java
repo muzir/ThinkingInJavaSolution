ec31b275d60896c2a2ff5a4b972bec890a0ef730
muzir <baycelik_erhun@hotmail.com>
2014-08-27 01:11:49 +0300

    Add new tests at App.java

--- src/main/java/org/muzir/book/solution/App.java
+++ src/main/java/org/muzir/book/solution/App.java
@@ -1,5 +1,9 @@
 package org.muzir.book.solution;
 
+import static org.muzir.book.solution.AccessControl.debugOff.Exercise3.*;
+
+import org.muzir.book.solution.AccessControl.Exercise4;
+
 /**
  * Hello world!
  *
@@ -16,5 +20,10 @@ public class App {
 	public static void main(String[] args) {
 		InitialValue initialValue = new InitialValue();
 		System.out.println("f method return:" + initialValue.f(initialValue.i));
+		org.muzir.book.solution.AccessControl.Exercise1 exercise1 = new org.muzir.book.solution.AccessControl.Exercise1();
+		System.out.println("Exercise1 " + exercise1.getClass());
+		debug();
+		Exercise4 exercise4 = new Exercise4();
+		System.out.println("Age:" + exercise4.get_age());
 	}
 }
