356c12a9eccbe212e059689cf974d771b08cfc3a
muzir <baycelik_erhun@hotmail.com>
2014-12-09 23:48:25 +0200

    -- Finished Reusing Classes chapter.

--- src/main/java/org/muzir/book/solution/ReusingClasses/Exercise18.java
+++ src/main/java/org/muzir/book/solution/ReusingClasses/Exercise18.java
@@ -1,20 +1,40 @@
 package org.muzir.book.solution.ReusingClasses;
 
-public class Exercise18 {
+class Poppet {
+	private int i;
 
-	class Value {
-		public Value() {
-			System.out.println("Create new Value");
-		}
+	Poppet(int ii) {
+		i = ii;
 	}
 
-	class FinalData {
-		final Value v1 = new Value();
-		static final Value v19 = new Value();
+	@Override
+	public String toString() {
+		return "Poppet [i=" + i + "]";
+	}
+
+}
+
+public class Exercise18 {
+
+	private final int i = 0; // Initialized final
+	private final int j; // Blank final
+	private final Poppet p; // Blank final reference
+
+	// Blank finals MUST be initialized in the constructor:
+	public Exercise18() {
+		j = 1; // Initialize blank final
+		p = new Poppet(1); // Initialize blank final reference
+	}
+
+	public Exercise18(int x) {
+		j = x; // Initialize blank final
+		p = new Poppet(x); // Initialize blank final reference
 	}
 
 	public static void main(String[] args) {
-
+		Exercise18 exercise18 = new Exercise18();
+		Exercise18 exercise18WithParam = new Exercise18(47);
+		System.out.println(exercise18.p.toString());
 	}
 
 }
