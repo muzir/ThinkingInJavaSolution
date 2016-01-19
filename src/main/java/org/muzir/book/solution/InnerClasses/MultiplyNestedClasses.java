7d37265ae9244abc736ae64d76da7bdddf806647
muzir <baycelik_erhun@hotmail.com>
2016-01-20 00:04:45 +0200

    -- Multiple Nested Classes sample.

---
+++ src/main/java/org/muzir/book/solution/InnerClasses/MultiplyNestedClasses.java
@@ -0,0 +1,33 @@
+package org.muzir.book.solution.InnerClasses;
+
+class MNA {
+	private void f() {
+	}
+
+	class A {
+		private void g() {
+		};
+
+		public class B {
+			void h() {
+				g();
+				f();
+			}
+		}
+	}
+}
+
+/**
+ * @author erhun.baycelik
+ *
+ */
+public class MultiplyNestedClasses {
+
+	public static void main(String[] args) {
+		MNA mna = new MNA();
+		MNA.A manaa = mna.new A();
+		// manaa.g(); //can't invoke private method why?
+		MNA.A.B manaab = manaa.new B();
+		manaab.h();
+	}
+}
